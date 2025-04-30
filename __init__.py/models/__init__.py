61e065ab5a04a913a410cddb236232a29c5b2bc1
Lucas Lorensi <86373836+Lucas-Lorensi@users.noreply.github.com>
2025-04-30 13:27:37 -0300

    New commit

---
+++ __init__.py/models/__init__.py
@@ -0,0 +1,6 @@
+"""
+Pydantic models for the salary prediction API.
+"""
+from app.models.prediction import PredictionFeatures, SalaryPrediction
+
+__all__ = ["PredictionFeatures", "SalaryPrediction"]
